47ea5d7746436c9357b4bbeb0db053e9c8b5bcf0
ssumg <37644244+ssumg@users.noreply.github.com>
2018-04-18 21:58:47 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-Ball[] ball = new Ball[10];
+/*Ball[] ball = new Ball[10];*/
 
 function setup(){
   createCanvas(600, 400);
   background(0);
-  for(int i=0; i<10; i++)
-    ball[i] = new Ball();
+  /*for(int i=0; i<10; i++)
+    ball[i] = new Ball();*/
 }
 
 function draw(){
-
+  ellipse(300, 200, 50, 50);
   background(0);
-  for(int i=0; i<10; i++){
+ /* for(int i=0; i<10; i++){
     ball[i].move();
     ball[i].display();
-  }
+  }*/
 }
-class Ball{
+/*class Ball{
   float x, y;
   float xDir, yDir;
   
@@ -42,4 +42,4 @@ class Ball{
     fill(y*x/2, x/2, x/y);
     ellipse(x, y, 50, 50);
     }
-}
+}*/
